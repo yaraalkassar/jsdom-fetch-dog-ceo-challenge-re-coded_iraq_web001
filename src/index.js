bf5ea579f090e2b8a8b7e8b1140b27a8d6760b53
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 20:33:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -16,40 +16,13 @@ function getBreed() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
   return fetch(breedUrl)
   .then(response => response.json())
-  .then((dogs) => {
-       dogs.forEach((dog) => {
-         image = document.createElement('img');
-             image.src = dog;
-             imageContainer.appendChild(image);
+  .then((breed) => {
+       breed.forEach((dogBreed) => {
+
        })
      })
 }
 
-
-  return fetch(`https://api.github.com/search/users?q=${inputVal.value}`)
-    .then((response) => response.json())
-    .then((profile) => {
-      profile.items.forEach((user) => {
-        let listItem = document.createElement("li");
-        listItem.innerHTML = `<b>${user.login}</b>
-         <div style="margin:20px"> <img src="${user.avatar_url}" alt="avatar" height="150px">
-</div>
-          <div style="margin:20px">
-          <a href="${user.html_url}">Profile Link</a>
-          </div>
-          <div style="margin:20px">
-          <button type="button" class="repo-btn">Click Me to show Repos!</button>
-          </div>
-          `;
-        let btn = listItem.querySelector(".repo-btn");
-        btn.addEventListener("click", function () {
-          searchRepo(user);
-        });
-        userList.appendChild(listItem);
-      });
-      inputVal.value = "";
-    });
-}
 
 
 
