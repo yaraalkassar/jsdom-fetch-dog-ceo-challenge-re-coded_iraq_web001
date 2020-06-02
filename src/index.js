7698ca8009e68d021db260d1d64462a04bc7e333
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 21:57:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -22,19 +22,19 @@ function getBreed() {
    fetch(breedUrl)
    .then(response => response.json())
    .then(result => {
-     let breedContainer = document.querySelector('#dog-breeds');
+     let breedList = document.querySelector('#dog-breeds');
      let breedDropdown = document.querySelector('#breed-dropdown');
      for (const key in result.message){
        let breed = document.createElement('li');
        breed.classList.add('breed')
        breed.innerText = key;
-       breedContainer.appendChild(breed);
+       breedList.appendChild(breed);
        breed.addEventListener('click', () => breed.style.color = 'red')
 
        breedDropdown.addEventListener('change', (event) => {
          breed.remove();
          if (breed.innerText.charAt(0) == event.target.value){
-           breedContainer.appendChild(breed);
+           breedList.appendChild(breed);
          }
        })
      }
