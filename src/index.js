ffd893a387e20165d998fdb779e3bc617318147d
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 19:46:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+//new code
+document.addEventListener('DOMContentLoaded', (event) => {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+let imgContainer = document.getElementById('dog-image-container');
+fetch(imgUrl)
+.then(response => response.json())
+then(data =>{
+  for(const img of data.)
+}};
