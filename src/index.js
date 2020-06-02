a49de720ad4680ac1f93ded36d775b9650f78a82
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 20:16:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@
 function getDog(){
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 let imgContainer = document.getElementById('dog-image-container');
-fetch(imgUrl)
+ return fetch(imgUrl)
 .then(response => response.json())
 .then((dogs) => {
       dogs.forEach((dog) => {
         image = document.createElement('img');
             image.src = dog;
             imageContainer.appendChild(image);
-      })})
+      })
+    })
 }
 function getBreed() {
   const breedUrl = 'https://dog.ceo/api/breeds/list/all'
