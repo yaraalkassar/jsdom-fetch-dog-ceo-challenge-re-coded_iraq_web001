14ec15d5f5ecaf1cfc23eec2fcdf98ba483abee1
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 20:35:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -26,33 +26,7 @@ return fetch(breedUrl)
 }
 
 
-  return fetch(`https://api.github.com/search/users?q=${inputVal.value}`)
-    .then((response) => response.json())
-    .then((profile) => {
-      profile.items.forEach((user) => {
-        let listItem = document.createElement("li");
-        listItem.innerHTML = `<b>${user.login}</b>
-         <div style="margin:20px"> <img src="${user.avatar_url}" alt="avatar" height="150px">
-</div>
-          <div style="margin:20px">
-          <a href="${user.html_url}">Profile Link</a>
-          </div>
-          <div style="margin:20px">
-          <button type="button" class="repo-btn">Click Me to show Repos!</button>
-          </div>
-          `;
-        let btn = listItem.querySelector(".repo-btn");
-        btn.addEventListener("click", function () {
-          searchRepo(user);
-        });
-        userList.appendChild(listItem);
-      });
-      inputVal.value = "";
-    });
-
-
-
-
+  
 
 
 
