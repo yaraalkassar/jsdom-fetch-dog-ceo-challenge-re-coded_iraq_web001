04fb921d1482d164ec4d195b854e6c653e1b3723
Yara Alkassar <yara.sarmad@yahoo.com>
2020-06-02 20:07:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 //new code
-document.addEventListener('DOMContentLoaded', (event) => {
+function getDog(){
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 let imgContainer = document.getElementById('dog-image-container');
 fetch(imgUrl)
 .then(response => response.json())
 .then((dogs) => {
-        dogs.forEach((dog) => {
-          image = document.createElement('img');
-              image.src = dog;
-              imageContainer.appendChild(image);
-        })
-}};
+      dogs.forEach((dog) => {
+        image = document.createElement('img');
+            image.src = dog;
+            imageContainer.appendChild(image);
+      })}
+}
+function getBreed() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+}
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+};
